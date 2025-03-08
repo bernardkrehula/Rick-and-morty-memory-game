9cb6766bac96c1e95ca4d4de1feeb4648b180346
bernardkrehula <krehula.bernard456@gmail.com>
2025-03-08 17:04:00 +0100

    Making game title html

---
+++ script.js
@@ -0,0 +1,5 @@
+//Napravi array objekata svaki objekat neka ima sljedeca polja id, name, img razmisli treba li jos nesto
+//Napravi kreatorske funkcije koje ti trebaju
+//Treba ce mi cardCreator i managerCreator
+//Za svaki objekat u arrayu pozovi cardCreator i novonastali objekat gurni u nastali managerArray
+//Od toga trenutka za sve koristis te nove pojacane objekte (manager.getCard(), card.getImg())
