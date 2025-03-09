6a8263cdd632a40bd404514c50a1304da198b3c2
bernardkrehula <krehula.bernard456@gmail.com>
2025-03-09 13:16:18 +0100

    Display objects on screen clousure

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
 const gameIcon = document.querySelectorAll('.game-icon');
 const gameIcons = document.querySelector('.icons')
 
-const iconsArray = [
-    {id: '', name: 'Gear Head', img: 'images/gearHeadpng.png'},
-    {id: '', name: 'Squanchy', img: 'images/squanchy.a39194b73a966ab61d06.png'},
-    {id: '', name: 'Snuffles', img: 'images/snuffles.120ea6e983a68aef7be5.png'},
-    {id: '', name: 'Krombopulos Michael', img: 'images/michael.8c9961c6a42e6577b366.png'},
-    {id: '', name: 'Coach Feratu', img: 'images/vampire.4c634082ab10e93076ac.png'},
-    {id: '', name: 'Bird Person', img: 'images/birdPerson.bbeb4909b0b4a608592c.png'},
-    {id: '', name: 'Prince Nebulon', img: 'images/princeNebulon.492189f9013fb3a2c907.png'},
-    {id: '', name: 'Scarry Terry', img: 'images/Terry.0005c5c221a69a614d0a.png'},
-    {id: '', name: 'Doofus Rick', img: 'images/J19ick.13cdd5d095ba92ee1ce4.png'},
-    {id: '', name: 'President Curtis', img: 'images/president.d8927b18ea37085c4685.png'},
-    {id: '', name: 'Abrodolph Lincoler', img: 'images/abraham.5bab582d158024aecc0a.png'},
-    {id: '', name: 'Rick Sanchez', img: 'images/rickkk.c06ec7790deb77a3746b.png'}
-]
+
 
 function managerCreator(){
-    let icon;
+
+    const iconsArray = [
+        {id: '', name: 'Gear Head', img: 'images/gearHeadpng.png'},
+        {id: '', name: 'Squanchy', img: 'images/squanchy.a39194b73a966ab61d06.png'},
+        {id: '', name: 'Snuffles', img: 'images/snuffles.120ea6e983a68aef7be5.png'},
+        {id: '', name: 'Krombopulos Michael', img: 'images/michael.8c9961c6a42e6577b366.png'},
+        {id: '', name: 'Coach Feratu', img: 'images/vampire.4c634082ab10e93076ac.png'},
+        {id: '', name: 'Bird Person', img: 'images/birdPerson.bbeb4909b0b4a608592c.png'},
+        {id: '', name: 'Prince Nebulon', img: 'images/princeNebulon.492189f9013fb3a2c907.png'},
+        {id: '', name: 'Scarry Terry', img: 'images/Terry.0005c5c221a69a614d0a.png'},
+        {id: '', name: 'Doofus Rick', img: 'images/J19ick.13cdd5d095ba92ee1ce4.png'},
+        {id: '', name: 'President Curtis', img: 'images/president.d8927b18ea37085c4685.png'},
+        {id: '', name: 'Abrodolph Lincoler', img: 'images/abraham.5bab582d158024aecc0a.png'},
+        {id: '', name: 'Rick Sanchez', img: 'images/rickkk.c06ec7790deb77a3746b.png'}
+    ];
+
     const cardCreator = () => {
        for(let i = 0; i < iconsArray.length; i++){
-        icon = iconsArray[i].img;
-        console.log(iconsArray[i].img)
-        pushGameIconOnScreen()
+        pushGameIconOnScreen(iconsArray[i]);
        }
     }
+    const getImg = (img) => {};
 
-    const pushGameIconOnScreen = () =>{
+    const pushGameIconOnScreen = (icon) =>{
         let html = 
         `
         <div class="game-icon">
-            <img src="${icon}">
+            <img src="${icon.img}">
+            <h1>${icon.name}</h1>
         </div>
         `;
         gameIcons.insertAdjacentHTML('beforeend', html);
     }
-    return { cardCreator, pushGameIconOnScreen }
+    return { cardCreator, pushGameIconOnScreen, getImg }
 }
 const manager = managerCreator();
 manager.cardCreator();
-
+console.log(manager.getImg())
 //Napravi array objekata svaki objekat neka ima sljedeca polja id, name, img razmisli treba li jos nesto (treba mi njezino stanje da znam je li bila kliknuta ili nije)
 //Napravi kreatorske funkcije koje ti trebaju
 //Treba ce mi cardCreator i managerCreator
